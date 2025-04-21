a948a8534a1f4bbda0f0c85f92975278e8882e6a
Hatim Thayyil <hatim@thayyil.net>
2025-04-21 19:36:09 +0100

    remove dep on flake-parts

--- flake.nix
+++ flake.nix
@@ -2,27 +2,12 @@
   description = "Emacs configuration";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-
-      ];
-
-      systems = [ "x86_64-linux" ];
-
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-
-      };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-        homeManagerModules.default = import ./nix/home-module.nix;
-      };
+  outputs = inputs@{ nixpkgs, ... }:
+    {
+      homeManagerModules.default = import ./nix/home-module.nix;
     };
 }
 
